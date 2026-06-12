8ce9ee862b0abb7dcbb2a63090931b91799a8d37
Jonathan Ferreira <jhouus33@gmail.com>
2026-06-11 23:12:05 -0300

    feat: add novas libs

--- LibFT/libs/ft_memchr.c
+++ LibFT/libs/ft_memchr.c
@@ -1,3 +1,22 @@
-O que faz: Vasculha os primeiros n bytes do bloco de memória s procurando pela primeira ocorrência do caractere c (tratado como unsigned char). Retorna um ponteiro para o byte encontrado ou um ponteiro nulo (0/NULL) se não achar nada.
+// O que faz: Vasculha os primeiros n bytes do bloco de memória s procurando pela primeira ocorrência do caractere c (tratado como unsigned char). Retorna um ponteiro para o byte encontrado ou um ponteiro nulo (0/NULL) se não achar nada.
 
-O que resolve: Assim como o memcpy é a versão "bruta" do strcpy, o memchr é a versão "bruta" do strchr. Ele resolve a necessidade de procurar um byte específico em dados binários ou blocos de memória que contenham zeros no meio, ignorando as regras convencionais de strings.
+// O que resolve: Assim como o memcpy é a versão "bruta" do strcpy, o memchr é a versão "bruta" do strchr. Ele resolve a necessidade de procurar um byte específico em dados binários ou blocos de memória que contenham zeros no meio, ignorando as regras convencionais de strings.
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *ptr_str;
+	size_t	i;
+
+	ptr_str = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr_str[i] == (unsigned char)c)
+		{
+			return ((void *)&ptr_str[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- LibFT/libs/libft.h
+++ LibFT/libs/libft.h
@@ -24,7 +24,7 @@ char	*ft_strdup(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
-
-void	ft_bzero(void *s, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
